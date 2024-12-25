f3a2ce8d091e06dcf9d0f72fd38001ecea04a89b
Murali <murali15062003@gmail.com>
2024-12-25 10:02:20 +0530

    Updating the Loading feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import DetailPage from "./Component/DetailPage";
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("https://restcountries.com/v3.1/all");
         const data = await response.json();
         setData(data);
+        setLoading(false);
         console.log(data[0]);
       } catch (err) {
         console.log("error occured..!");
@@ -21,10 +23,14 @@ function App() {
   return (
     <div className="bg-lightbg dark:bg-darkbg min-h-screen">
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Body data={data} />} />
-        <Route path="/:country" element={<DetailPage data={data} />} />
-      </Routes>
+      {loading ? (
+        <h1 className='md:text-5xl text-center font-semibold text-lighttext dark:text-darktext mt-5'>Loading..!</h1>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Body data={data} />} />
+          <Route path="/:country" element={<DetailPage data={data} />} />
+        </Routes>
+      )}
     </div>
   );
 }
